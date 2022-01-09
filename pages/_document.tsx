5bfa73066572ad7c8bfe534da69cb3cedb1e4cb0
Peter Cardenas <petercardenas441@gmail.com>
2022-01-09 00:23:39 -0600

    feat: add ff din font

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preload" href="/fonts/DIN.ttf" as="font" crossOrigin="" />
+          <link
+            rel="preload"
+            href="/fonts/DIN-Black.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/DIN-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/DIN-Bold.ttf"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Din", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
