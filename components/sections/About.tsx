1adf6c58f62f6b7665c6df1e5178ef9d4ea6b376
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-01-12 06:08:58 -0600

    feat: about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,11 +1,51 @@
-import {about} from "utils/profile.json";
+import Image from "next/image";
+import MailIcon from "components/icons/Mail";
+import GithubIcon from "components/icons/Github";
+import LinkedinIcon from "components/icons/LinkedIn";
+import profile from "utils/profile.json";
 
 const About = () => {
-  return <div className="h-screen w-screen">
-    <div className="w-1/2 max-w-5xl flex flex-row justify-between">
-      <div className="flex flex-col"><h1>Hi! I'm {about.bio}</h1></div>
+  const { about } = profile;
+  return (
+    <div className="h-screen w-screen">
+      <div className="h-1/4" />
+      <div className="w-1/2 max-w-5xl flex flex-row justify-between m-auto">
+        <div className="flex flex-col mr-16">
+          <h1 className="mt-10 mb-10">Hi! I&apos;m {about.firstName}.</h1>
+          <p>{about.bio}</p>
+          <div className="flex flex-row mt-5">
+            <a
+              className="mr-6 transition hover:-translate-y-1"
+              href={about.linkedinUrl}
+            >
+              <LinkedinIcon />
+            </a>
+            <a
+              className="mr-6 transition hover:-translate-y-1"
+              href={about.githubUrl}
+            >
+              <GithubIcon />
+            </a>
+            <a
+              className="transition hover:-translate-y-1"
+              href={`mailto:${about.email}`}
+            >
+              <MailIcon />
+            </a>
+          </div>
+        </div>
+        <div className="flex-grow flex-shrink-0">
+          <Image
+            className="w-full"
+            src={about.profilePic}
+            width={256}
+            height={256}
+            alt="Profile Picture"
+          />
+        </div>
+      </div>
     </div>
-  </div>;
-}
+  );
+};
 
 export default About;
