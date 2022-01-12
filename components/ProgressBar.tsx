fc4764f0e611ebda8895d8a87fdf4f8b845df685
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-01-12 06:54:17 -0600

    feat: add progress bar

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,7 +1,19 @@
 const ProgressBar = () => {
   return (
-    <div className="h-screen w-screen">
-      <div></div>
+    <div className="fixed bottom-0 right-8">
+      <div className="flex flex-row">
+        <div className="text-right flex flex-col justify-between pb-16 mr-[26px]">
+          <p>About</p>
+          <p>Experience</p>
+          <p>Work</p>
+          <p>Contact</p>
+        </div>
+        <div className="pt-1">
+          <div className="relative bg-black w-1 h-96 rounded-t-lg">
+            <div className="absolute top-0 bg-alternate w-6 h-6 rounded-xl -left-2.5" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "components/sections/About";
+import ProgressBar from "components/ProgressBar";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <About />
+      <ProgressBar />
     </div>
   );
 };
