28d2cfd1f732ea2f5200985ed31b7b8d58f835a7
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-01-12 05:54:37 -0600

    format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import About from 'components/sections/About'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import About from "components/sections/About";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Peter's Portfolio</title>
+        <title>Peter&apos;s Portfolio</title>
         <meta name="description" content="View some of my work!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <About />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
