8d3c6d8d91ac045dbff044d8b4f4b5ff63805d25
Peter Cardenas <petercardenas441@gmail.com>
2022-01-14 01:41:36 -0600

    fix: testing linked text styling

--- components/LinkedText.tsx
+++ components/LinkedText.tsx
@@ -14,7 +14,7 @@ const LinkedText = ({ children, href }: LinkedTextProps) => {
           <span className="relative">
             {element}
             {index != elements.length - 1 && " "}
-            <span className="-z-10 absolute transition-all left-0 bottom-1 w-full h-1.5 group-hover:h-2 bg-alternate opacity-50 group-hover:opacity-75" />
+            <span className="-z-10 absolute transition-all left-0.5 bottom-1 w-[calc(100%+4px)] h-1.5 group-hover:h-2 bg-alternate opacity-50 group-hover:opacity-75" />
           </span>
         </Fragment>
       ))}
