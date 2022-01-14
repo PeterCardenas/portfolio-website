1e5a1c24677e4af661358342dc78e16c8360b061
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-01-13 22:04:48 -0600

    feat: add gradient background for progress bar and fix zindex

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -34,82 +34,100 @@ const ProgressBar = ({
   }, [isMobile]);
 
   return (
-    <div className="fixed bottom-0 right-8">
-      <div className="flex flex-row justify-end">
-        <div className="text-right mr-[26px]">
-          {sections.map((section, index) => {
-            const sectionLabel = section.toLowerCase();
-            const isActive = sectionLabel === activeSection;
-            return (
-              <div key={section} className="flex justify-end">
-                <div
-                  className={cx(
-                    "group hover:text-alternate cursor-default lg:cursor-pointer transition-all relative",
-                    {
-                      "text-alternate": isActive,
-                      "mb-24": index !== sections.length - 1 && showMenu,
-                      "mb-0": index !== sections.length - 1 && !showMenu,
-                      "mt-0": index === 0 && showMenu,
-                      "mt-[372px]": index === 0 && !showMenu,
-                    }
-                  )}
-                  onClick={() => scrollToSection(sectionLabel)}
-                >
-                  <p
-                    className={cx("transition-all", {
-                      "text-2xl": showMenu,
-                      "text-[0px]": !showMenu,
-                    })}
+    <>
+      <div
+        className={cx(
+          "fixed z-50 bottom-0 right-0 transition-all duration-1000 opacity-80",
+          {
+            "w-screen h-screen bg-gradient-to-l from-whiter to-alternate":
+              showMenu,
+            "w-0 h-0 bg-transparent": !isMobile,
+          }
+        )}
+      />
+      <div className="fixed z-50 bottom-16 right-8">
+        <div className="flex flex-row justify-end">
+          <div className="text-right mr-[26px]">
+            {sections.map((section, index) => {
+              const sectionLabel = section.toLowerCase();
+              const isActive = sectionLabel === activeSection;
+              return (
+                <div key={section} className="flex justify-end">
+                  <div
+                    className={cx(
+                      "group hover:text-alternate cursor-default lg:cursor-pointer transition-all relative",
+                      {
+                        "text-alternate": isActive,
+                        "mb-24": index !== sections.length - 1 && showMenu,
+                        "mb-0": index !== sections.length - 1 && !showMenu,
+                        "mt-0": index === 0 && showMenu,
+                        "mt-[372px]": index === 0 && !showMenu,
+                      }
+                    )}
+                    onClick={() => {
+                      scrollToSection(sectionLabel);
+                      if (isMobile && sectionLabel !== activeSection) {
+                        setTimeout(() => setShowMenu(false), 750);
+                      }
+                    }}
                   >
-                    {section}
-                  </p>
-                  <div className="absolute z-10 -right-10 top-1 w-6 h-6 flex items-center justify-center">
-                    <div
-                      className={cx(
-                        "bg-alternate rounded-xl transition-all",
-                        showMenu && {
-                          "w-0 h-0 group-hover:w-6 group-hover:h-6": !isActive,
-                          "w-6 h-6": isActive,
-                        }
-                      )}
-                    />
+                    <p
+                      className={cx("transition-all", {
+                        "text-2xl": showMenu,
+                        "text-[0px]": !showMenu,
+                      })}
+                    >
+                      {section}
+                    </p>
+                    <div className="absolute z-10 -right-10 top-1 w-6 h-6 flex items-center justify-center">
+                      <div
+                        className={cx(
+                          "bg-alternate rounded-xl transition-all",
+                          showMenu && {
+                            "w-0 h-0 group-hover:w-6 group-hover:h-6":
+                              !isActive,
+                            "w-6 h-6": isActive,
+                          }
+                        )}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="relative h-[500px] w-1 mt-1 mb-16">
-          <div
-            className={cx(
-              "absolute bottom-0 bg-black transition-all w-full rounded-t-lg",
-              {
-                "h-full": showMenu,
-                "h-0": !showMenu,
-              }
-            )}
-          >
-            <div
-              className="absolute top-0 left-0 w-full bg-alternate transition-all rounded-t-lg"
-              style={{ height: showMenu ? `${progress}px` : 0 }}
-            />
+              );
+            })}
           </div>
+          <div className="relative h-[500px] w-1 mt-1">
+            <div
+              className={cx(
+                "absolute bottom-0 bg-black transition-all w-full rounded-t-lg",
+                {
+                  "h-full": showMenu,
+                  "h-0": !showMenu,
+                }
+              )}
+            >
+              <div
+                className="absolute top-0 left-0 w-full bg-alternate transition-all rounded-t-lg"
+                style={{ height: showMenu ? `${progress}px` : 0 }}
+              />
+            </div>
 
-          <div
-            className={cx(
-              "absolute bottom-0 -left-2.5 rounded-xl w-6 h-6 bg-black flex justify-center items-center transition cursor-pointer",
-              {
-                "-rotate-90 hover:rotate-90": showMenu,
-                "rotate-90 hover:-rotate-90": !showMenu,
-              }
-            )}
-            onClick={() => setShowMenu((showMenu) => !showMenu)}
-          >
-            <DoubleChevronRight theme="white" />
+            <div
+              className={cx(
+                "absolute bottom-0 -left-2.5 rounded-xl w-6 h-6 bg-black flex justify-center items-center transition cursor-pointer",
+                {
+                  "-rotate-90 hover:rotate-90": showMenu,
+                  "rotate-90 hover:-rotate-90": !showMenu,
+                }
+              )}
+              onClick={() => setShowMenu((showMenu) => !showMenu)}
+            >
+              <DoubleChevronRight theme="white" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
